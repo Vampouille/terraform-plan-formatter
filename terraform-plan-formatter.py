57ce3c533cbb593571d3048ad44f5eacc32bcffd
Julien Acroute <julien.acroute@camptocamp.com>
2017-06-19 17:12:34 +0200

    Report changes count and add --quiet option to only report changes count

--- terraform-plan-formatter.py
+++ terraform-plan-formatter.py
@@ -3,7 +3,15 @@
 import sys, re
 from enum import Enum
 from difflib import unified_diff
+import argparse
 
+parser = argparse.ArgumentParser(description='Parse output of terraform plan and report changes and stats')
+parser.add_argument('-q', '--quiet', help='Report count of changes instead of human readable diff',
+        action='store_true', default=False)
+args = parser.parse_args()
+
+# this var is used to count diff between current state and wanted state
+diff_count = 0
 
 stack_list_item_re = re.compile('^rancher_stack\.([a-zA-Z-]+):\sRefreshing\sstate...')
 stack_diff_header_re = re.compile('^(~|\+|-)\srancher_stack\.([a-zA-Z-]+)$')
@@ -25,60 +33,73 @@ class State(Enum):
 
 
 def parse_stack_list_line(line):
-    # print("List line : %s" % line)
+    # _print("List line : %s" % line)
     pass
 
+def _print(line=''):
+    if not args.quiet:
+        print(line)
 
 def parse_compose_diff_line(line, diff_type, stack_name):
-    print('''######################
+    _print('''######################
 # %s %s
 ######################
     ''' % (convert_diff_type[diff_type], stack_name))
     line = line.strip()
     if diff_type != '~':
         return
-    # print("Parsing : %s" % line)
+    # _print("Parsing : %s" % line)
     matches = extract_diff_re.match(line)
     item = matches.group(1)
     diff1 = matches.group(2).replace('\\n', '\n').splitlines()
     diff2 = matches.group(3).replace('\\n', '\n').splitlines()
     diff = unified_diff(diff1, diff2, n=100000)
-    print("* %s:\n----------\n" % item)
+    _print("* %s:\n----------\n" % item)
     i = 1
     for line in diff:
         if i > 3:
-            print(line)
+            _print(line)
         i += 1
-    print()
+    _print()
+    # update diff count
+    global diff_count
+    local_diff_count = 0
+    for line in unified_diff(diff1, diff2, n=0):
+        local_diff_count += 1
+    diff_count = diff_count + max(0, local_diff_count - 3)
 
 def parse_catalog_env_diff_line(line, diff_type, stack_name):
-    #print("Catalog env line %s" % line)
+    #_print("Catalog env line %s" % line)
     matches = catalog_env_diff_re.match(line)
     var = matches.group(1)
     old_value = matches.group(2)
     new_value = matches.group(3)
     if var == '%':
-        print('''######################
+        _print('''######################
 # Modifying catalog env %s
 ######################
 ''' % stack_name)
-    print("%s: %s => %s" % (var, old_value, new_value))
+    _print("%s: %s => %s" % (var, old_value, new_value))
+    global diff_count
+    diff_count += 1
 
 def parse_catalog_id_diff_line(line, diff_type, stack_name):
     matches = catalog_id_diff_re.match(line)
     old_value = matches.group(1)
     new_value = matches.group(2)
-    print('''######################
+    _print('''######################
 # Modifying catalog version %s
 ######################
 %s => %s''' % (stack_name, old_value, new_value))
+    global diff_count
+    diff_count += 1
 
 state = State.BEFORE_STACK_LIST
 
 for line in sys.stdin:
     # remove trailling new line
     line = line.strip("\n")
-    # print("%s : %s" % (state, line))
+    # _print("%s : %s" % (state, line))
 
     if state == State.BEFORE_STACK_LIST:
         if stack_list_item_re.match(line) is not None:
@@ -103,8 +124,12 @@ for line in sys.stdin:
         elif compose_diff_re.match(line) is not None:
             parse_compose_diff_line(line, diff_type, current_stack_name)
         else:
-            print("")
+            if not args.quiet:
+                _print()
             state = State.AFTER_STACK_LIST
 
-
+if not args.quiet:
+    print("Total diff count : %s" % diff_count)
+else:
+    print(diff_count)
 
